c342ab31bb2c086f8393a98fdfdaac7ef6005f5d
Sergey <sergey.ponomarenko1@gmail.com>
2021-10-07 12:56:27 +0300

    [ZP 0.6] Создание классов в Zampa.Entities
    Создание классов:
    1. Identification
    2. Information
    Добавление свойств классов

---
+++ Zampa/Zampa.Entities/Common/Identification.cs
@@ -0,0 +1,9 @@
+﻿namespace Zampa.Entities.Common
+{
+    public class Identification
+    {
+        public int ID { get; set; }
+
+        public string APIKey { get; set; }
+    }
+}
---
+++ Zampa/Zampa.Entities/Common/Information.cs
@@ -0,0 +1,9 @@
+﻿namespace Zampa.Entities.Common
+{
+    public class Information
+    {
+        public string Request { get; set; }
+
+        public string Responce { get; set; }
+    }
+}
